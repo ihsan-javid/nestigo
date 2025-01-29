54ba45a2f5de3376d15e88d72c81b29990b779f2
ihsan-javid <ihsanjavid01@gmail.com>
2025-01-29 23:07:48 +0500

    Prepare for deployment

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const MongoStore = require("connect-mongo");
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
-const User = require("./model/user.js");
+const User = require("./models/user.js");
 
 const reviewRoute = require("./routes/reviews.js");
 const listingRoute = require("./routes/listings.js");
